4a369756128d4881a48c28b41ad18fd7dca95f93
Seleena Dates <admin@seleenadates.com>
2026-01-17 10:50:33 +0200

    Add push-to-github script

---
+++ push-to-github.sh
@@ -0,0 +1,14 @@
+# Push to GitHub Script
+# Run these commands after creating the repository on GitHub
+
+# Step 1: Add the remote repository
+git remote add origin https://github.com/abrhmann/seleena-dates.git
+
+# Step 2: Rename branch to main (GitHub's default)
+git branch -M main
+
+# Step 3: Push your code to GitHub
+git push -u origin main
+
+# After running these commands, your repository will be live at:
+# https://github.com/abrhmann/seleena-dates
